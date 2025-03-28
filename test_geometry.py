e495c457009e7d8d026c40f7b84f6c60993b4fa9
ElviraSenalova <elvirasenalova@gmail.com>
2025-03-29 00:39:04 +0600

    Ломаем тест для проверки CI

--- test_geometry.py
+++ test_geometry.py
@@ -2,4 +2,6 @@
 from geometry import calculate_area
 
 def test_calculate_area():
-    assert calculate_area(5, 10) == 100
+    assert calculate_area(5, 10) == 50
+    assert calculate_area(0, 10) == 0
+    assert calculate_area(3.5, 2) == 7.0
